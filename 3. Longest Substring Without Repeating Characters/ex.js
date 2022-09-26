3030ee8b6b3140028f551b0ea2994d3a16465dfa
Margherita <63791162+RavaMarghe@users.noreply.github.com>
2022-09-26 16:08:55 +0200

    Add ex 3

---
+++ 3. Longest Substring Without Repeating Characters/ex.js
@@ -0,0 +1,28 @@
+function lengthOfLongestSubstring(s) {
+  
+  if (!s) {
+    return 0;
+  }
+  
+  let start = 0;
+  
+  let end = 0;
+  
+  let maxLength = 0;
+ 
+  const uniqueCharacters = new Set();
+  
+  while (end < s.length) {
+    if (!uniqueCharacters.has(s[end])) {
+      uniqueCharacters.add(s[end]);
+      end++;
+      maxLength = Math.max(maxLength, uniqueCharacters.size);
+    } else {
+      uniqueCharacters.delete(s[start]);
+      start++;
+    }
+  }
+  return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
