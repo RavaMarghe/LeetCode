9dadaa04ee49f7c9a6adb0b8bf6299ab954a3f63
Margherita <63791162+RavaMarghe@users.noreply.github.com>
2022-09-27 13:57:47 +0200

    Add ex 14

--- 3. Longest Substring Without Repeating Characters/ex.js
+++ 3. Longest Substring Without Repeating Characters/ex.js
@@ -1,17 +1,16 @@
 function lengthOfLongestSubstring(s) {
-  
   if (!s) {
     return 0;
   }
-  
+
   let start = 0;
-  
+
   let end = 0;
-  
+
   let maxLength = 0;
- 
+
   const uniqueCharacters = new Set();
-  
+
   while (end < s.length) {
     if (!uniqueCharacters.has(s[end])) {
       uniqueCharacters.add(s[end]);
@@ -26,3 +25,5 @@ function lengthOfLongestSubstring(s) {
 }
 
 console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("bbbbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
